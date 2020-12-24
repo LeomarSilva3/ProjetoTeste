fccbe85f771eecdeb9d518636553e4b1064b4c29
Leomar da Silva Barbosa <leomar_barbosa@terceiros.sicredi.com.br>
2020-12-24 09:57:54 -0300

    Adicionando SingleUser DTO's

---
+++ src/main/java/dto/SingleUserResponseDTO.java
@@ -0,0 +1,17 @@
+package dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class SingleUserResponseDTO {
+
+    private DataDTO data;
+    private SupportDTO support;
+
+}
---
+++ src/main/java/dto/SupportDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SupportDTO {
+
+    private String url;
+    private String text;
+
+}
