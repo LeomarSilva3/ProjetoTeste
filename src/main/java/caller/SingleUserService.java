8066253d9b2277518e51f43b4191e638143460c1
Leomar da Silva Barbosa <leomar_barbosa@terceiros.sicredi.com.br>
2020-12-24 10:48:20 -0300

    Criando chamada do serviço requestSpecification na BaseTest

---
+++ src/main/java/caller/SingleUserService.java
@@ -0,0 +1,26 @@
+package caller;
+
+import common.BaseTest;
+import dto.SingleUserRequestDTO;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+
+import static enums.Enums.*;
+import static io.restassured.RestAssured.given;
+
+public class SingleUserService extends BaseTest {
+
+        public ValidatableResponse getUserId(SingleUserRequestDTO request){
+            return
+                    given().
+                            spec(requestSpecification).
+                            pathParam("userId",request.getUserId()).
+                            when().
+                            get(SINGLE_USER.getUrl()).
+                            then().
+                            statusCode(HttpStatus.SC_OK).
+                            contentType(ContentType.JSON);
+        }
+
+}
