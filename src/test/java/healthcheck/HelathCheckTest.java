2a642475d6011382f4df52a2b396682f786dc7b2
LeomarSilva3 <43186522+LeomarSilva3@users.noreply.github.com>
2020-12-27 11:43:48 -0300

    Update HelathCheckTest.java
    Atualizando healthCheckTest

--- src/test/java/healthcheck/HelathCheckTest.java
+++ src/test/java/healthcheck/HelathCheckTest.java
@@ -22,6 +22,6 @@ public class HelathCheckTest extends BaseTest {
             then().
                statusCode(SC_OK).
                body("status", is("UP")).
-               contentType(ContentType.JSON);
+               contentType(ContentType.HTML);
         }
 }
